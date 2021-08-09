b5b14626935078b3c8ac076c53b5d3d94f62928f
vithiyasahar <vithiyasaharvigneswaran@gmail.com>
2021-08-09 18:16:07 +0530

    2204 Assignment

---
+++ src/Question.scala
@@ -0,0 +1,20 @@
+object Question {
+    def main(args: Array[String]): Unit = {
+      val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      val E=(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)+key)%a.size);      //Encryption function
+      val D=(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)-key)%a.size);      //Decryption function
+      val cipher=(algo:(Char,Int,String)=> Char,s:String,key:Int,a:String)=>s.map(algo(_,key,a));  // Higher order function
+      print("Enter the string that needs to be encrypted: ");
+      var str= scala.io.StdIn.readLine();
+      val ct=cipher(E,str,5,alphabet);
+      println("Encrypted String: "+ct);                                           // Encrypted string
+      val pt=cipher(E,ct,5,alphabet);
+      println("Decrypted String: "+str);                                         // Decrypted string
+
+
+    }
+
+  }
+
+
+
